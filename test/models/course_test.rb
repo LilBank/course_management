adbd53655f5c2f529359cef9acbfd6b13ef9edc5
bankkeez <sethitikul@gmail.com>
2020-05-18 17:53:02 +0700

    add unit tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
+require 'test_helper'
+
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get course(s) from a single instructor" do
+    assert_equal ["Spy course"], users(:user_007).courses.map { |course| course.name }
+  end
+
+  test "should get courses from multiple instructors" do
+    assert_equal ["Cooking","P.E.","Math"], users(:user_003).courses.map { |course| course.name }
+  end
+
+  test "should get courses from a single student" do
+    assert_equal ["Spy course","Cooking","P.E."], users(:user_002).courses.map { |course| course.name }
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get a single user id" do
+    assert_equal 1, users(:user_007).id
+  end
+
+  test "should get multiple users id" do
+    assert_equal 1, users(:user_007).id
+    assert_equal 2, users(:user_001).id
+    assert_equal 3, users(:user_002).id
+  end
 end
